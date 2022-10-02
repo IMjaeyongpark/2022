12d22920e918a78811f95ea9ecdb2703248de7dc
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-10-02 12:17:35 +0900

    Update 1002.py

--- 2-2/DASC/1002.py
+++ 2-2/DASC/1002.py
@@ -15,3 +15,47 @@ def personal_info(**info):
 
 x = {'name': '홍길동','age':30,'address':'서울시 용산구 이촌동'}
 personal_info(**x)
+
+#01 별찍기
+
+for i in range(5):
+    print(' '*(4-i),'*'*(2*(i+1)-1))
+    
+#02 최대 최소
+def get_max_min(data_list):
+    return min(data_list), max(data_list)
+
+lst = list(map(int,input().split()))
+_min,_max = get_max_min(lst)
+print('최소: {0}, 최대: {1}'.format(_min,_max))
+
+#03 1부터 n까지 제곱의 합
+def  sum_sq(num):
+    sum = 0
+    for i in range(1,num+1):
+        sum+=i*i
+    return sum
+
+print(sum_sq(int(input())))
+
+#04 약수 구하기
+def sol(num):
+    lst = []
+    for i in range(1,num+1):
+        if num%i==0:
+            lst.append(i) 
+    return lst
+
+print(sol(int(input())))
+
+#05 1부터 10,000까지 8의 갯수
+def count8(num):
+    cnt = 0
+    for i in range(num+1):
+        tmp = str(i)
+        for n in tmp:
+            if n == '8':
+                cnt+=1
+    return cnt
+
+print(count8(10000))
